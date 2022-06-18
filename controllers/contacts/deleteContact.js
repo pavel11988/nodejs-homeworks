6d9d73cf7c39fee1a0ea4fafe3a7f0ebc9e1eb51
pavel11988 <kossoua88@gmail.com>
2022-06-18 20:51:33 +0300

    Done

---
+++ controllers/contacts/deleteContact.js
@@ -0,0 +1,19 @@
+const contactsOperations = require("../../models/contacts");
+const { createError } = require("../../helpers");
+
+const deleteContact = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contactsOperations.removeContact(contactId);
+    if (!result) {
+      throw createError(404, "Not found");
+    }
+    res.status(200).json({
+      message: "Contact deleted",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = deleteContact;
